c0766958cc92301da71f673858ec4795c0bd0da4
MedAliFD <medalifaddaoui@gmail.com>
2024-03-16 18:16:18 +0100

    FirstJSX

---
+++ week2/firstJSX/firstjsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2> things I want to do: </h2>
+    <ol>
+      <li> learn react</li>
+      <li> climb Mt. Everest</li>
+      <li> Run a marathon</li>
+      <li> Feed the dogs</li>
+    </ol>
+    </div>
+  );
+}
+
+export default App;
