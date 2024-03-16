a70d0f703e632c0863cfca9744127510dfaed94f
MedAliFD <medalifaddaoui@gmail.com>
2024-03-16 16:43:24 +0100

    Ninja

--- week1/Ninja/ninja.js
+++ week1/Ninja/ninja.js
@@ -1,25 +1,39 @@
-
-
 class Ninja {
     constructor(name, health, strength, speed) {
         this.name = name;
-        this.health = health;
-        this.strength = strength;
-        this.speed = speed;
+        this.health = 90;
+        this.strength = 3;
+        this.speed = 3;
     }
 
     sayName() {
         console.log(`My ninja name is ${this.name}`);
+        //! return this; // so we can add multiple methode in same line
+
     }
 
     showStats() {
-        console.log(`Name: ${this.name}, Health: ${this.health}, Strength: ${this.strength}, Speed: ${this.speed}`);
+        console.log(`
+        Name: ${this.name},
+        Health: ${this.health}, 
+        Strength: ${this.strength}, 
+        Speed: ${this.speed}`);
+        //! return this; // so we can add multiple methode in same line
+
+    }
+    drinkSake(){
+        console.log("hp boost activated +10")
+        this.health += 10
+        //! return this; // so we can add multiple methode in same line
+
     }
 }
 
-const ninja1 = new Ninja("Hyabusa", 100 , 10 , 10 );
-const ninja2 = new Ninja("zed", 100 , 10 , 20 );
+const ninja1 = new Ninja("zed");
 
-ninja2.sayName(); 
-ninja2.showStats(); 
+ninja1.sayName(); 
+ninja1.showStats(); 
+ninja1.drinkSake();
+ninja1.showStats();
+//! ninja1.sayName().showStats().drinkSake().showStats(); // so we can add multiple methode in same line
 
