d5a78cd7cdb0b3cb2539b9854f28693942df33f6
MedAliFD <medalifaddaoui@gmail.com>
2024-04-25 15:30:42 +0100

    Joke API

--- week3/luke-api/src/componenets/Search.jsx
+++ week3/luke-api/src/componenets/Search.jsx
@@ -24,6 +24,7 @@ function Search() {
         <div>
           <label htmlFor="">Search for : </label>
           <select onChange={(e) => setSearchFor(e.target.value)}>
+            <option></option>
             <option>planets</option>
             <option>people</option>
           </select>
